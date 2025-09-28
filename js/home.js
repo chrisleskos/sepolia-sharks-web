8b6977c652d5f0febd5563c168d3ffbb2085fb6a
chrisleskos <chrisles143@gmail.com>
2025-09-29 01:32:03 +0300

    updates home page and mostly next match view

--- js/home.js
+++ js/home.js
@@ -3,6 +3,13 @@ totalImages = 4;
 imagesPath = "img/header/";
 let intervalId;
 
+let globalNextMatchYear;
+let globalNextMatchMonth;
+let globalNextMatchDate;
+let globalNextMatchHour;
+let globalNextMatchMinute;
+let globalCountdownIntervalId;
+
 function imageChange() {
   headerImage = document.getElementById("header-img");
   headerImage.src = imagesPath + "h" + (counter + 1) + ".jpg";
@@ -41,35 +48,49 @@ function checkOrientation() {
 }
 
 fetch("/.netlify/functions/schedule")
-  .then((res) => res.json())
+  .then((res) => {
+    if (!res.ok) {
+      // catches 4xx/5xx
+      throw new Error(`HTTP ${res.status}`);
+    }
+    return res.json();
+  })
   .then((data) => {
     console.log("Earliest game:", data);
     setNextGame(data);
   })
-  .catch((err) => console.error("Fetch error:", err));
+  .catch((err) => {
+    setNoGame();
+    console.error("Fetch error:", err);
+  });
 
 function setNextGame(json) {
   if (json.error) return setNoGame();
-  let ctaNextMatch = document.getElementsByClassName(
-    "cta-text cta-full-next-match-text"
-  )[0];
-  let nextMatchDetailsWrap = document.getElementsByClassName(
-    "full-next-match-details-wrap"
-  )[0];
-
   let nextMatchCompetition = document.getElementsByClassName(
+    "next-match-competition"
+  )[0];
+  let nextMatchOpposingTeamImg = document.querySelectorAll(
+    ".next-match-opposing-team > img"
+  )[0];
+  let nextMatchOpposingTeamName = document.querySelectorAll(
+    ".next-match-opposing-team > div"
+  )[0];
+  let nextMatchDate = document.getElementsByClassName("next-match-date")[0];
+
+  // Full Next Match selectors
+  let fullNextMatchCompetition = document.getElementsByClassName(
     "full-next-match-competition"
   )[0];
-  let nextMatchRound = document.getElementsByClassName(
+  let fullNextMatchRound = document.getElementsByClassName(
     "full-next-match-round"
   )[0];
-  let nextMatchTeam = document.getElementsByClassName(
+  let fullNextMatchTeam = document.getElementsByClassName(
     "full-next-match-team-name"
   )[0];
-  let nextMatchDate = document.getElementsByClassName(
+  let fullNextMatchDate = document.getElementsByClassName(
     "full-next-match-date"
   )[0];
-  let nextMatchPlace = document.getElementsByClassName(
+  let fullNextMatchPlace = document.getElementsByClassName(
     "full-next-match-place"
   )[0];
 
@@ -104,7 +125,8 @@ function setNextGame(json) {
   ];
   const splittedDate = json.date.split("/");
   const day = splittedDate[0];
-  const month = months[splittedDate[1] - 1];
+  const monthNumber = splittedDate[1];
+  const month = months[monthNumber - 1];
   const monthGR = monthsGR[splittedDate[1] - 1];
   const year = splittedDate[2].substring(0, 4);
   const time = splittedDate[2]
@@ -123,32 +145,72 @@ function setNextGame(json) {
     "GR"
   );
 
-  // set cta date
-  // ctaNextMatch.innerHTML = `${dayOfWeekGR} ${day} ${monthGR} ${year}, <span >${time}</span>`;
-  // set team image
-  // nextMatchDetailsWrap.style.backgroundImage = `url(${json.teamImage})`;
-  nextMatchCompetition.innerHTML = `<span>${json.competition}</span>`;
-  nextMatchRound.innerText = "Game " + json.round;
-  nextMatchTeam.innerText = json.teamName;
-  nextMatchDate.innerHTML = `${month} ${day}, ${dayOfWeek} <span class='neon-time'>${time}</span>`;
-  nextMatchPlace.innerHTML = `<img src="img/auxilary/navigate.png" /><div>${json.place}</div>`;
-  nextMatchPlace.onclick = () => {
+  // set global variables for countdown
+
+  globalNextMatchYear = year;
+  globalNextMatchMonth = monthNumber;
+  globalNextMatchDate = day;
+  globalNextMatchHour = time.substring(1, 3);
+  globalNextMatchMinute = time.substring(4, time.length);
+
+  globalCountdownIntervalId = setInterval(updateCountdown, 1000);
+
+  // set next game
+  nextMatchCompetition.innerHTML = json.competition;
+  nextMatchOpposingTeamImg.src = json.teamImage;
+  nextMatchOpposingTeamName.innerHTML = json.teamName;
+  nextMatchDate.innerHTML = `${month} ${day}, ${dayOfWeek.substring(
+    0,
+    3
+  )} <span class='neon-time'>${time}</span>`;
+
+  // set Full next game
+  fullNextMatchCompetition.innerHTML = `<span>${json.competition}</span>`;
+  fullNextMatchRound.innerText = "Game " + json.round;
+  fullNextMatchTeam.innerText = json.teamName;
+  fullNextMatchDate.innerHTML = `${month} ${day}, ${dayOfWeek} <span class='neon-time'>${time}</span>`;
+  fullNextMatchPlace.innerHTML = `<img src="img/auxilary/navigate.png" /><div>${json.place}</div>`;
+  fullNextMatchPlace.onclick = () => {
     window.open(`https://www.google.com/maps/search/${json.place}`, "_blank");
   };
 }
 
 function setNoGame() {
-  let nextMatchDetailsWrap = document.getElementsByClassName(
+  let fullNextMatchDetailsWrap = document.getElementsByClassName(
     "full-next-match-details-wrap"
   )[0];
 
-  // let ctaNextMatch = document.getElementsByClassName(
-  //   "cta-text cta-full-next-match-text"
-  // )[0];
+  let nextMatchTeams = document.getElementsByClassName("next-match-teams")[0];
+  let nextMatchCompetition = document.getElementsByClassName(
+    "next-match-competition"
+  )[0];
 
-  nextMatchDetailsWrap.innerHTML = `<div class="no-match">Δεν υπάρχουν πληροφορίες για το επόμενο παιχνίδι...</div>`;
-  ctaNextMatch.innerHTML =
-    "Δεν υπάρχουν πληροφορίες για το επόμενο παιχνίδι...";
+  fullNextMatchDetailsWrap.innerHTML = `<div class="no-match">Δεν υπάρχουν πληροφορίες για το επόμενο παιχνίδι...</div>`;
+
+  nextMatchTeams.innerHTML = `<div class="no-match">Δεν υπάρχουν πληροφορίες για το επόμενο παιχνίδι...</div>`;
+  nextMatchCompetition.style.display = "none";
+  clearInterval(globalCountdownIntervalId);
+}
+
+function updateCountdown() {
+  // Countdown
+  let nextMatchDaysCountdown = document.getElementById("days-counter");
+  let nextMatchHoursCountdown = document.getElementById("hours-counter");
+  let nextMatchMinutesCountdown = document.getElementById("minutes-counter");
+  let nextMatchSecondsCountdown = document.getElementById("seconds-counter");
+
+  const [daysDiff, hoursDiff, minDiff, secDiff] = getCountdown(
+    globalNextMatchYear,
+    globalNextMatchMonth,
+    globalNextMatchDate,
+    globalNextMatchHour,
+    globalNextMatchMinute
+  );
+
+  nextMatchDaysCountdown.innerHTML = daysDiff;
+  nextMatchHoursCountdown.innerHTML = hoursDiff;
+  nextMatchMinutesCountdown.innerHTML = minDiff;
+  nextMatchSecondsCountdown.innerHTML = secDiff;
 }
 
 function zellerDayOfWeek(year, month, day, lang = "EN") {
@@ -195,4 +257,27 @@ function zellerDayOfWeek(year, month, day, lang = "EN") {
   }
 
   return days[h];
+}
+
+function getCountdown(year, month, date, hour, minute) {
+  let today = new Date();
+  let gameDay = new Date(`${year}-${month}-${date}T${hour}:${minute}:00`);
+
+  let dateDiff = Math.abs(today - gameDay) / 1000; // total seconds
+
+  const daysDiff = Math.floor(dateDiff / (3600 * 24));
+  dateDiff -= daysDiff * 3600 * 24;
+
+  const hoursDiff = Math.floor(dateDiff / 3600);
+  dateDiff -= hoursDiff * 3600;
+
+  const minDiff = Math.floor(dateDiff / 60);
+  const secDiff = Math.floor(dateDiff % 60);
+
+  return [
+    String(daysDiff).padStart(2, "0"),
+    String(hoursDiff).padStart(2, "0"),
+    String(minDiff).padStart(2, "0"),
+    String(secDiff).padStart(2, "0"),
+  ];
 }
